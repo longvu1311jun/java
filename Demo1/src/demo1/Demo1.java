4c7ed7e6398ee6d7b11d6b66eca368b78470b489
long-vu5664 <78557381+long-vu5664@users.noreply.github.com>
2023-03-07 16:53:30 +0700

    hello

---
+++ Demo1/src/demo1/Demo1.java
@@ -0,0 +1,16 @@
+package demo1;
+
+/**
+ *
+ * @author DELL
+ */
+public class Demo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Helo");
+    }
+    
+}

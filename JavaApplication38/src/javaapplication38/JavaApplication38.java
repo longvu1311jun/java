8d83b4e6402f973e1204693e0a3a6e9a8fbd1f4b
long-vu5664 <78557381+long-vu5664@users.noreply.github.com>
2023-03-07 17:04:40 +0700

    push

---
+++ JavaApplication38/src/javaapplication38/JavaApplication38.java
@@ -0,0 +1,16 @@
+package javaapplication38;
+
+/**
+ *
+ * @author DELL
+ */
+public class JavaApplication38 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hh");
+    }
+    
+}
